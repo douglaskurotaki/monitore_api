2c5474d488d2bd206a4a549e7721027f4fb55dee
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2021-07-28 17:52:06 -0300

    Remove protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,5 @@
 
 class ApplicationController < ActionController::API
   respond_to :json
-  protect_from_forgery with: :null_session, if: -> { request.format.json? }
   include DeviseTokenAuth::Concerns::SetUserByToken
 end
