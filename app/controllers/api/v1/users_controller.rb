e48ad107503bfa192efa72b48611cf54ea5a0f91
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2021-07-28 17:52:58 -0300

    Add modules api v1 to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
-class UsersController < ApplicationController
-  def create
-    @user = User.new(user_params)
-    @user.save
-    render json: @user
-  end
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def create
+        @user = User.new(user_params)
+        @user.save
+        render json: @user
+      end
 
-  private
+      private
 
-  def user_params
-    params.permit(:email, :password, :password_confirmation)
+      def user_params
+        params.permit(:email, :password, :password_confirmation)
+      end
+    end
   end
 end
