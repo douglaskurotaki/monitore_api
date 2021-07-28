f33095e9ae9c5f3f66ae4fcdc9e334a5dfe64c9a
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2021-07-28 17:51:17 -0300

    Add logger

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,6 @@ module Monitore
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.mongoid.logger = Logger.new($stdout, :warn)
   end
 end
