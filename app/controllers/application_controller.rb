cb2429b03bbdcbea4a2cc3623a7f6d9935cd0745
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2021-07-30 06:46:34 -0300

    Add strong params to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  respond_to :json
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  respond_to :json
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name nickname])
+  end
 end
